8a671565cbb8a9e05547321ef3fe908bf045e462
Framonti <37839292+Framonti@users.noreply.github.com>
2021-01-29 14:11:20 +0100

    URLs

--- yt_download/youtube_playlist_downloader.py
+++ yt_download/youtube_playlist_downloader.py
@@ -6,25 +6,40 @@ from dotenv import load_dotenv
 
 from config import YT_DOWNLOAD_ABSOLUTE_PATH
 
-load_dotenv()
 
-with open(os.path.join(YT_DOWNLOAD_ABSOLUTE_PATH, 'yt_playlists.txt'), 'r') as playlist_file:
-    for playlist_url in playlist_file.read().split("\n"):
-        query = parse_qs(urlparse(playlist_url).query, keep_blank_values=True)
-        playlist_id = query["list"][0]
-        print(f'get all playlist video links from {playlist_id}')
-        youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=os.getenv('DEVELOPER_KEY'))
-        request = youtube.playlistItems().list(part="snippet", playlistId=playlist_id, maxResults=50)
-        response = request.execute()
+def remove_duplicates():
+    lines_seen = set()  # holds lines already seen
+    with open(os.path.join(YT_DOWNLOAD_ABSOLUTE_PATH, 'yt_urls.txt'), "w") as output_file:
+        for each_line in open(os.path.join(YT_DOWNLOAD_ABSOLUTE_PATH, 'yt_urls_duplicates.txt'), "r"):
+            if each_line not in lines_seen:  # check if line is not duplicate
+                output_file.write(each_line)
+                lines_seen.add(each_line)
 
-        playlist_items = []
 
-        while request is not None:
+def urls_from_playlists():
+    with open(os.path.join(YT_DOWNLOAD_ABSOLUTE_PATH, 'yt_playlists.txt'), 'r') as playlist_file:
+        for playlist_url in playlist_file.read().split("\n"):
+            query = parse_qs(urlparse(playlist_url).query, keep_blank_values=True)
+            playlist_id = query["list"][0]
+            print(f'get all playlist video links from {playlist_id}')
+            youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=os.getenv('DEVELOPER_KEY'))
+            request = youtube.playlistItems().list(part="snippet", playlistId=playlist_id, maxResults=50)
             response = request.execute()
-            playlist_items += response["items"]
-            request = youtube.playlistItems().list_next(request, response)
 
-        print(f"total: {len(playlist_items)}")
-        with open(os.path.join(YT_DOWNLOAD_ABSOLUTE_PATH, 'yt_urls.txt'), 'a+') as url_file:
-            for item in playlist_items:
-                url_file.write(f'https://www.youtube.com/watch?v={item["snippet"]["resourceId"]["videoId"]}\n')
+            playlist_items = []
+
+            while request is not None:
+                response = request.execute()
+                playlist_items += response["items"]
+                request = youtube.playlistItems().list_next(request, response)
+
+            print(f"total: {len(playlist_items)}")
+            with open(os.path.join(YT_DOWNLOAD_ABSOLUTE_PATH, 'yt_urls_duplicates.txt'), 'a+') as url_file:
+                for item in playlist_items:
+                    url_file.write(f'https://www.youtube.com/watch?v={item["snippet"]["resourceId"]["videoId"]}\n')
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    urls_from_playlists()
+    remove_duplicates()
